a482390d371bf7d3cea490e63ffb9fbb8be3ecc5
A283le <alexdominguez169@gmail.com>
2017-11-10 17:59:23 -0500

    First Major Mile Stone

--- src/rootkit.c
+++ src/rootkit.c
@@ -1,13 +1,30 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/proc_fs.h>
-#include <linux/string.h>
-#include <linux/cred.h>
-#include <linux/fs.h>
+#include <linux/sched.h>
+#include <linux/errno.h>
 
-static int __init rootkit_init(void){;}
-static void __exit rootkit_exit(void){;}
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Malware Team");
+MODULE_DESCRIPTION("A rootkit");
+MODULE_VERSION("1.0.0");
+MODULE_SUPPORTED_DEVICE("None");
+
+struct task_struct *task;
+
+static int __init rootkit_init(void){
+	printk(KERN_INFO "Hello World!");
+
+	for_each_process(task){
+		printk(KERN_INFO "Task id: %d, Priority: %d", task->pid, task->rt_priority);
+	}
+
+	return 0;
+}
+
+static void __exit rootkit_exit(void){
+	printk(KERN_INFO "Goodbye World!");
+}
 
 module_init(rootkit_init);
 module_exit(rootkit_exit);
